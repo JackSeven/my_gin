b653c6c11a9cc4627fe6fa2401755a3bd9723bed
wj <704910054@qq.com>
2020-03-24 21:54:17 +0800

    goland push

--- main.go
+++ main.go
@@ -1,61 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-//type LoginInfo struct {
-//	Username string	`json:"username"`
-//	Userpwd string `json:"userpwd"`
-//}
-
-//数据绑定 接收json 类型数据
-
-//func main() {
-//
-//	router := gin.Default()
-//
-//	router.POST("/submit", func(c *gin.Context) {
-//		var json LoginInfo
-//		// 接受json , 用postman 的 json 类型调试
-//		if err := c.ShouldBindJSON(&json); err != nil {
-//			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-//			return
-//		}
-//
-//		if json.Username != "admin" || json.Userpwd != "123456" {
-//			c.JSON(http.StatusBadRequest, gin.H{"error": "用户名或密码错误"})
-//			return
-//		}
-//
-//		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": "", "msg":"success"})
-//	})
-//
-//	router.Run(":6001")
-//}
-//
-
-
-// 数据绑定 接收 form 表单提交数据
-
-type LoginInfo struct {
-	Username string	`form:"username"`
-	Userpwd string `form:"userpwd"`
-}
-
 func main() {
 	router := gin.Default()
 
-	router.POST("/submit", func(c *gin.Context) {
+	//router.LoadHTMLGlob("view/*")
 
-		var form LoginInfo
-		if err := c.Bind(&form); err != nil {
-			c.JSON(http.StatusOK, gin.H{"error":err.Error()})
+	router.LoadHTMLFiles("view/index.html")
+
+	router.GET("/index", func(c *gin.Context) {
+		id := c.Query("id")
+		if id == "12" {
+			c.Redirect(http.StatusMovedPermanently, "http://www.baidu.com")
 		}
 
-		c.JSON(http.StatusOK, gin.H{"username": form.Username, "userpwd": form.Userpwd})
-		
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title":   "文章标题",
+			"content": fmt.Sprintf("文章内容%s", id),
+		})
 	})
 
 	router.Run(":6001")
